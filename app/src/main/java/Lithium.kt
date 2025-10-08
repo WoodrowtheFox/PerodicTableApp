bee3f46fb46537327479b5135bb184185f84f4d3
Woodrow <thnlundgren@gmail.com>
2025-10-08 16:15:24 -0500

    Finished adding all elements

---
+++ app/src/main/java/Lithium.kt
@@ -0,0 +1,96 @@
+class Lithium : Elements() {
+    override val elementname: String?
+        /**
+         * This is used to get the elements name as a string
+         *
+         * @return - the element name
+         */
+        get() = "Lithium"
+
+    override val atomicnumber: Int
+        /**
+         * This is used to get the atomic number of an element
+         *
+         * @return - the atomic number
+         */
+        get() = 3
+
+    override val atomicmass: Float
+        /**
+         * This is used to get the atomic mass of an element
+         *
+         * @return - the atomic mass
+         */
+        get() = 7.0f
+
+    override val groupnumber: Int
+        /**
+         * This is used to get the group number of an element
+         *
+         * @return - the group number
+         */
+        get() = 1
+
+    /**
+     * This is used to get the number of valence elctrons
+     *
+     * @return - returns the number of valence electrons
+     */
+    public override fun getvalenceelectrons(): Int {
+        return 1
+    }
+
+    override val periodnumber: Int
+        /**
+         * This is used to get the periodic number of an element
+         *
+         * @return - the periodic number
+         */
+        get() = 2
+
+    override val familyname: String?
+        /**
+         * This is used to get the family name of an element
+         *
+         * @return - the family name
+         */
+        get() = "Alkali Metal"
+
+    /**
+     * This is used to get the common uses of an element
+     *
+     * @return - the common uses
+     */
+    public override fun getcommonuses(): String? {
+        return "Batteries"
+    }
+
+    /**
+     * This is used to get the ionic state of the element
+     *
+     * @return - the ionic state
+     */
+    public override fun getionicstate(): Int {
+        return 1
+    }
+
+    override val imagename: String?
+        /**
+         * This is used to get the image name of an element
+         *
+         * @return - the image name
+         */
+        get() = "Li-base.png"
+
+    /**
+     * This is used to get how many electrons are in each shell of an element
+     *
+     * @return - returns a hashnmap of all the shells
+     */
+    public override fun getshelltotals(): HashMap<String?, Int?> {
+        val myhash = HashMap<String?, Int?>()
+        myhash.put("1s", 2)
+        myhash.put("2s", 1)
+        return myhash
+    }
+}
---
+++ app/src/main/java/Titanium.kt
@@ -0,0 +1,104 @@
+/**
+ * This is for the data of Titanium
+ */
+class Titanium : Elements() {
+    override val elementname: String?
+        /**
+         * This is used to get the elements name as a string
+         *
+         * @return - the element name
+         */
+        get() = "Titanium"
+
+    override val atomicnumber: Int
+        /**
+         * This is used to get the atomic number of an element
+         *
+         * @return - the atomic number
+         */
+        get() = 22
+
+    override val atomicmass: Float
+        /**
+         * This is used to get the atomic mass of an element
+         *
+         * @return - the atomic mass
+         */
+        get() = 47.867f
+
+    override val groupnumber: Int
+        /**
+         * This is used to get the group number of an element
+         *
+         * @return - the group number
+         */
+        get() = 4
+
+    /**
+     * This is used to get the number of valence elctrons
+     *
+     * @return - returns the number of valence electrons
+     */
+    public override fun getvalenceelectrons(): Int {
+        return 2
+    }
+
+    override val periodnumber: Int
+        /**
+         * This is used to get the periodic number of an element
+         *
+         * @return - the periodic number
+         */
+        get() = 4
+
+    override val familyname: String?
+        /**
+         * This is used to get the family name of an element
+         *
+         * @return - the family name
+         */
+        get() = "Transition Metal"
+
+    /**
+     * This is used to get the common uses of an element
+     *
+     * @return - the common uses
+     */
+    public override fun getcommonuses(): String? {
+        return "Aerospace, Artificial Joints"
+    }
+
+    /**
+     * This is used to get the ionic state of the element
+     *
+     * @return - the ionic state
+     */
+    public override fun getionicstate(): Int {
+        return 3
+    }
+
+    override val imagename: String?
+        /**
+         * This is used to get the image name of an element
+         *
+         * @return - the image name
+         */
+        get() = "Ti-base.png"
+
+    /**
+     * This is used to get how many electrons are in each shell of an element
+     *
+     * @return - returns a hashnmap of all the shells
+     */
+    public override fun getshelltotals(): HashMap<String?, Int?> {
+        val myhash = HashMap<String?, Int?>()
+        myhash.put("1s", 2)
+        myhash.put("2s", 2)
+        myhash.put("2p", 6)
+        myhash.put("3s", 2)
+        myhash.put("3p", 6)
+        myhash.put("3d", 2)
+        myhash.put("4s", 2)
+        return myhash
+    }
+}
